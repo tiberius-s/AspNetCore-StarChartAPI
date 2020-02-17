3f2a3b5e7a33b0288cd52e20e28493011d434de8
Tiberius Silivestru <tiberiussilivestru@northwesternmutual.com>
2020-02-17 12:33:26 -0600

    added CelestialObject class

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
